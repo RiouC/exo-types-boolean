cbfd4af7d4e00b6b34d503002d63629f63fc12a3
RiouC <riou.christophe@protonmail.com>
2021-02-15 16:15:17 +0100

    coat : add coat.js; improve program with temperature

---
+++ coat.js
@@ -0,0 +1,11 @@
+// Programme qui va m'aider à décider si je dois mettre mon manteau
+// ou pas en fonction d'une méteo pluvieuse ou venteuse
+let isRainy = false;
+let isWindy = false;
+let isCold  = false;
+
+if (isRainy || isWindy || isCold) {
+    console.log('Mettez votre manteau !');
+} else {
+  console.log('Nous n\'avez pas besoin de manteau aujourd\'hui.');
+}
